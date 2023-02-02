169c8bba5013e2c3f9b4b199af12f6a07fde5d87
Matt Cieslak <mattcieslak@gmail.com>
2023-02-02 18:53:31 -0500

    use correct jupyter command

--- combine_subcortical.sh
+++ combine_subcortical.sh
@@ -7,12 +7,13 @@ datalad run \
   -i "transforms/tpl-MNI152NLin6Asym_from-MNI152NLin2009cAsym_mode-image_xfm.h5" \
   -i "transforms/tpl-MNI152NLin6Asym_from-MNI152NLin6Sym_mode-image_xfm.h5" \
   -i "JointAtlas.ipynb" \
+  -o "JointAtlas.ipynb" \
   -o "tpl-MNI152NLin2009cAsym_atlas-SubcorticalMerged_res-01_dseg.nii.gz" \
   -o "tpl-MNI152NLin2009cAsym_atlas-SubcorticalMerged_res-01_dseg.tsv" \
   -o "tpl-MNI152NLin6Asym_atlas-SubcorticalMerged_res-01_dseg.nii.gz" \
   -o "tpl-MNI152NLin6Asym_atlas-SubcorticalMerged_res-01_dseg.tsv" \
   -o "schaefer_map/*_dseg.nii.gz" \
   -o "schaefer_map/*_dseg.tsv" \
-  "jupyter run JointAtlas.ipynb"
+  "jupyter nbconvert --to notebook --execute --allow-errors JointAtlas.ipynb"
 
 
