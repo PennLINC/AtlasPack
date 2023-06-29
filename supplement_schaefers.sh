7a078e2dc6995e7acfe274ac3903edd3286580b4
Matt Cieslak <mattcieslak@gmail.com>
2023-06-29 14:31:32 -0400

    scripts for volumetric atlases

---
+++ supplement_schaefers.sh
@@ -0,0 +1,15 @@
+datalad run \
+  --explicit \
+  --expand both \
+  -m "add merged subcortical atlas to Schaefer parcellations" \
+  -i "tpl-MNI152NLin6Asym_atlas-SubcorticalMerged_res-01_dseg.nii.gz" \
+  -i "tpl-MNI152NLin2009cAsym_atlas-SubcorticalMerged_res-01_dseg.nii.gz" \
+  -i "tpl-MNI152NLin2009cAsym_atlas-SubcorticalMerged_res-01_dseg.tsv" \
+  -i "schaefer_map/tpl-MNI152NLin6Asym_atlas-Schaefer2018v0143_res-01_desc-*dseg.nii.gz" \
+  -i "schaefer_map/tpl-MNI152NLin2009cAsym_atlas-Schaefer2018v0143_res-01_desc-*dseg.nii.gz" \
+  -i "schaefer_map/atlas-Schaefer2018v0143_desc-*ParcelsAllNetworks_dseg.tsv" \
+  -i "atlas-4S*Parcels_dseg.tsv" \
+  -o "tpl-MNI152NLin*_atlas-4S*Parcels_res-01_dseg.nii.gz" \
+  "jupyter nbconvert --to notebook --execute --allow-errors JointAtlas.ipynb"
+
+
